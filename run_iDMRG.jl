9b5b1dcc72eb98da173c99653023fc4f458d71b9
Philipp Schmoll <philipp.schmoll@uni-mainz.de>
2021-02-18 09:28:29 +0100

    minor changes to running script

--- run_iDMRG.jl
+++ run_iDMRG.jl
@@ -19,17 +19,19 @@ Base.run(`clear`)
 tol = 1e-6
 steps = 10
 
-# model settings Ising
-setSym = "Z2"
-J = 1.0
-h = 0.5
-mpo = mpoIsing(J = J, h = h, setSym = setSym)
-
-# # model settings Heisenberg
-# setSym = ""
-# J = 1.0
-# spinS = 1/2;
-# mpo = mpoHeisenberg(J = J, spinS = spinS, setSym = setSym)
+# choose model
+setModel = "TFIM";
+if setModel == "TFIM"
+    setSym = "Z2"
+    J = 1.0
+    h = 0.5
+    mpo = mpoIsing(J = J, h = h, setSym = setSym)
+elseif setModel == "HM"
+    setSym = "SU2"
+    J = 1.0
+    spinS = 1/2;
+    mpo = mpoHeisenberg(J = J, spinS = spinS, setSym = setSym)
+end
 
 @time psi = DMRG_engine.iDMRG2(mpo, χ=χ, steps=steps, tol=tol)
 # @time DMRG_engine.iDMRG2(χ=χ, maxNumSteps=100, setSym="Z2", tol=tol)
