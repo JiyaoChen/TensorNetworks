3f65aff17a5423990b8d8ff733535095668c3944
Philipp Schmoll <philipp.schmoll@uni-mainz.de>
2021-07-30 11:42:38 +0200

    CTMRG progress...

--- autodiff/iPEPS_multiSite.jl
+++ autodiff/iPEPS_multiSite.jl
@@ -158,27 +158,27 @@ function runCTMRG(iPEPS, chiE, truncBelowE, initMethod = 0)
     # get physical dimension
     d = size(iPEPS[1,1], 3);
 
-    # initialize variables to control CTMRG procedure
-    oldvals = fill(Inf, 4 * chiE * d);
-    tol = 0;
-    maxIter = 10;
+    # # initialize variables to control CTMRG procedure
+    # oldvals = fill(Inf, 4 * chiE * d);
+    # tol = 0;
+    # maxIter = 10;
 
-    # construct struct for stopFunction
-    stopFunc = StopFunction(oldvals, -1, tol, maxIter)
+    # # construct struct for stopFunction
+    # stopFunc = StopFunction(oldvals, -1, tol, maxIter)
 
     # make CTMRG step and return CRMTG tensors
-    CTMRGTensors = CTMRGStep((CTMRGTensors..., oldvals), (iPEPS, chiE, truncBelowE, d))[1 : end - 1];
+    CTMRGTensors = CTMRGStep(CTMRGTensors..., (iPEPS, chiE, truncBelowE, d));
     return CTMRGTensors
 
 end
 
-function CTMRGStep((C1, T1, C2, T2, C3, T3, C4, T4, oldvals), (iPEPS, chiE, truncBelowE, d))
-    C4, T4, C1 = absorptionStep_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE)
-    # C1, T1, C2 = absorptionStep_U(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE)
-    # C2, T2, C3 = absorptionStep_R(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE)
-    # C3, T3, C4 = absorptionStep_D(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE)
+function CTMRGStep(C1, T1, C2, T2, C3, T3, C4, T4, (iPEPS, chiE, truncBelowE, d))
+    
+    (oC1, oT1, oC2, oT2, oC3, oT3, oC4, oT4) = deepcopy.((C1, T1, C2, T2, C3, T3, C4, T4));
+    C4, T4, C1, C2, T2, C3 = absorptionStep_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE);
+    C1, T1, C2, C3, T3, C4 = absorptionStep_UD(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE);
 
-    return (C1, C2, C3, C4, T1, T2, T3, T4, oldvals)
+    return (C1, T1, C2, T2, C3, T3, C4, T4)
 end
 
 function absorptionStep_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE)
@@ -186,15 +186,43 @@ function absorptionStep_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBel
     projsUL, projsDL = similar.((T1, T3));
     projsUR, projsDR = similar.((T3, T1));
     nC4, nT4, nC1 = similar.((C4, T4, C1));
+    nC2, nT2, nC3 = similar.((C2, T2, C3));
 
     foreach(keys(iPEPS.tensorDict)) do tensorKey
         projsUL[tensorKey...], projsDL[tensorKey...], projsUR[tensorKey...], projsDR[tensorKey...] = computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE, tensorKey);
     end
+
+    (oC1, oT1, oC2, oT2, oC3, oT3, oC4, oT4) = deepcopy.((C1, T1, C2, T2, C3, T3, C4, T4));
     foreach(keys(iPEPS.tensorDict)) do tensorKey
-        nC4[tensorKey...], nT4[tensorKey...], nC1[tensorKey...] = absorption_L(C1, T1, T3, C4, T4, iPEPS, projsUL, projsDL, tensorKey);
+        @info tensorKey
+        absorption_L!(nC4, nT4, nC1, oC1, oT1, oT3, oC4, oT4, iPEPS, projsUL, projsDL, tensorKey);
+        absorption_R!(nC2, nT2, nC3, oT1, oC2, oT2, oC3, oT3, iPEPS, projsUR, projsDR, tensorKey);
     end
 
-    return nC4, nT4, nC1
+    return nC4, nT4, nC1, nC2, nT2, nC3
+
+end
+
+function absorptionStep_UD(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE)
+    
+    projsUL, projsUR = similar.((T3, T1));
+    projsDL, projsDR = similar.((T1, T3));
+    nC1, nT1, nC2 = similar.((C1, T1, C2));
+    nC3, nT3, nC4 = similar.((C3, T3, C4));
+
+    foreach(keys(iPEPS.tensorDict)) do tensorKey
+        projsUL[tensorKey...], projsUR[tensorKey...], projsDL[tensorKey...], projsDR[tensorKey...] = computeIsometries_UD(CTMRGTensors..., iPEPS, chiE, truncBelowE, tensorKey);
+    end
+
+    (oC1, oT1, oC2, oT2, oC3, oT3, oC4, oT4) = deepcopy.((C1, T1, C2, T2, C3, T3, C4, T4));
+    foreach(keys(iPEPS.tensorDict)) do tensorKey
+        @info tensorKey
+        absorption_U!(nC1, nT1, nC2, oT4, oC1, oT1, oC2, oT2, iPEPS, projsUL, projsUR, tensorKey);
+        absorption_D!(nC3, nT3, nC4, oT2, oC3, oT3, oC4, oT4, iPEPS, projsDL, projsDR, tensorKey);
+    end
+
+    return nC1, nT1, nC2, nC3, nT3, nC4
+
 end
 
 function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE, (idX, idY))
@@ -221,9 +249,6 @@ function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, trunc
     FUL = UU * sqrt(SU);
     FUR = sqrt(SU) * VU;
 
-    # reshape FUL and FUR
-    FUL = reshape(FUL, sizeRhoU[1], sizeRhoU[2], sizeRhoU[3], size(FUL, 2));
-    FUR = reshape(FUR, size(FUR, 1), sizeRhoU[4], sizeRhoU[5], sizeRhoU[6]);
 
 
     # perform SVD of rhoD
@@ -244,13 +269,10 @@ function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, trunc
     FDR = UD * sqrt(SD);
     FDL = sqrt(SD) * VD;
 
-    # reshape FDR and FUR
-    FDR = reshape(FDR, sizeRhoD[1], sizeRhoD[2], sizeRhoD[3], size(FDR, 2));
-    FDL = reshape(FUR, size(FDL, 1), sizeRhoD[4], sizeRhoD[5], sizeRhoD[6]);
 
 
     # compute biorthogonalization of FUL and FDL tensors and truncate UL, SL and VL
-    @ein BOL[-1, -2] := FDL[-1, 1, 2, 3] * FUL[1, 2, 3, -2];
+    BOL = FDL * FUL;
     BOL /= norm(BOL);
     UL, SL, VL = svd(BOL);
     SL = diagm(SL);
@@ -264,12 +286,16 @@ function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, trunc
     sqrtSL = sqrt(pinv(SL));
 
     # build projectors for left truncation
-    @ein PUL[-1, -2, -3, -4] := FUL[-1, -2, -3, 1] * adjoint(VL)[1, 2] * sqrtSL[2, -4];
-    @ein PDL[-1, -2, -3, -4] := FDL[1, -2, -3, -4] * adjoint(UL)[2, 1] * sqrtSL[2, -1];
+    PUL = FUL * VL' * sqrtSL;
+    PDL = sqrtSL * UL' * FDL;
+
+    # reshape PUL and PDL
+    PUL = reshape(PUL, sizeRhoU[1], sizeRhoU[2], sizeRhoU[3], size(PUL, 2));
+    PDL = reshape(PDL, size(PDL, 1), sizeRhoD[4], sizeRhoD[5], sizeRhoD[6]);
 
 
     # compute biorthogonalization of FUR and FDR tensors
-    @ein BOR[-1, -2] := FUR[-1, 1, 2, 3] * FDR[1, 2, 3, -2];
+    BOR = FUR * FDR;
     BOR /= norm(BOR);
     UR, SR, VR = svd(BOR);
     SR = diagm(SR);
@@ -283,8 +309,12 @@ function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, trunc
     sqrtSR = sqrt(pinv(SR));
 
     # build projectors for right truncation
-    @ein PUR[-1, -2, -3, -4] := sqrtSR[-1, 1] * adjoint(UR)[1, 2] * FUR[2, -2, -3, -4];
-    @ein PDR[-1, -2, -3, -4] := sqrtSR[-4, 2] * adjoint(VR)[1, 2] * FDR[-1, -2, -3, 1];
+    PUR = sqrtSR * UR' * FUR;
+    PDR = FDR * VR' * sqrtSR;
+
+    # reshape PUR and PDR
+    PUR = reshape(PUR, size(PUR, 1), sizeRhoU[4], sizeRhoU[5], sizeRhoU[6]);
+    PDR = reshape(PDR, sizeRhoD[1], sizeRhoD[2], sizeRhoD[3], size(PDR, 2));
 
 
     # return projectors
@@ -292,26 +322,162 @@ function computeIsometries_LR(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, trunc
 
 end
 
-function absorption_L!(nC1, nT1, nT3, nC4, nT4, C1, T1, T3, C4, T4, iPEPS, projsUL, projsDL, (x, y))
+function computeIsometries_UD(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, chiE, truncBelowE, (idX, idY))
     
+    # compute left and right half of the network
+    rhoL, rhoR = verticalCut(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, (idX, idY))
+    d = size(iPEPS[1,1], 3);
+
+    # perform SVD of rhoL
+    sizeRhoL = size(rhoL);
+    rhoL = reshape(rhoL, prod(sizeRhoL[1 : 3]), prod(sizeRhoL[4 : 6]));
+    rhoL /= norm(rhoL);        
+    UL, SL, VL = svd(rhoL);
+    SL = diagm(SL);
+    VL = VL';
+
+    # truncate singular values
+    newChi = sum(diag(SL) .> truncBelowE);
+    UL = UL[:, 1 : newChi];
+    SL = SL[1 : newChi, 1 : newChi];
+    VL = VL[1 : newChi, :];
+
+    # absorb sqrt(SL) into UL and VL
+    FDL = UL * sqrt(SL);
+    FUL = sqrt(SL) * VL;
+
+
+    # perform SVD of rhoR
+    sizeRhoR = size(rhoR);
+    rhoR = reshape(rhoR, prod(sizeRhoR[1 : 3]), prod(sizeRhoR[4 : 6]));
+    rhoR /= norm(rhoR);
+    UR, SR, VR = svd(rhoR);
+    SR = diagm(SR);
+    VR = VR';
+
+    # truncate singular values
+    newChi = sum(diag(SR) .> truncBelowE);
+    UR = UR[:, 1 : newChi];
+    SR = SR[1 : newChi, 1 : newChi];
+    VR = VR[1 : newChi, :];
+
+    # absorb sqrt(SR) into UR and VR
+    FUR = UR * sqrt(SR);
+    FDR = sqrt(SR) * VR;
+
+
+    # compute biorthogonalization of FUL and FUR tensors
+    BOU = FUL * FUR;
+    BOU /= norm(BOU);
+    UU, SU, VU = svd(BOU);
+    SU = diagm(SU);
+    VU = VU';
+
+    # truncate UU, SU and VU
+    newChi = min(chiE, sum(diag(SU) .> truncBelowE));
+    UU = UU[:, 1 : newChi];
+    SU = SU[1 : newChi, 1 : newChi];
+    VU = VU[1 : newChi, :];
+    sqrtSU = sqrt(pinv(SU));
+
+    # build projectors for up truncation
+    PUL = sqrtSU * UU' * FUL;
+    PUR = FUR * VU' * sqrtSU;
+    
+    # reshape PUL and PUR
+    PUL = reshape(PUL, size(PUL, 1), sizeRhoL[4], sizeRhoL[5], sizeRhoL[6]);
+    PUR = reshape(PUR, sizeRhoR[1], sizeRhoR[2], sizeRhoR[3], size(PUR, 2));
+    
+
+    # compute biorthogonalization of FDR and FDL tensors
+    BOD = FDR * FDL;
+    BOD /= norm(BOD);
+    UD, SD, VD = svd(BOD);
+    SD = diagm(SD);
+    VD = VD';
+
+    # truncate UD, SD and VD
+    newChi = min(chiE, sum(diag(SD) .> truncBelowE));
+    UD = UD[:, 1 : newChi];
+    SD = SD[1 : newChi, 1 : newChi];
+    VD = VD[1 : newChi, :];
+    sqrtSD = sqrt(pinv(SD));
+
+    # build projectors for right truncation
+    PDR = sqrtSD * UD' * FDR;
+    PDL = FDL * VD' * sqrtSD;
+
+    # reshape PDR and PDL
+    PDR = reshape(PDR, size(PDR, 1), sizeRhoR[4], sizeRhoR[5], sizeRhoR[6]);
+    PDL = reshape(PDL, sizeRhoL[1], sizeRhoL[2], sizeRhoL[3], size(PDL, 2));
+
+
+    # return projectors
+    return  PUL, PUR, PDL, PDR;
+
+end
+
+function absorption_L!(nC4, nT4, nC1, C1, T1, T3, C4, T4, iPEPS, projsUL, projsDL, (x, y))
+    
+    nC4[x + 0, y + 1] = ein"(cd, cafe), defb -> ab"(C4[x + 0, y + 0], T3[x + 0, y + 0], projsUL[x + 0, y + 0]);
+    nT4[x + 0, y + 1] = ein"(afkg, (fejh, egmbi), hlid), jkmcl -> abcd"(projsDL[x + 0, y + 0], T4[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), projsUL[x - 1, y + 0], iPEPS[x + 0, y + 0]);
     nC1[x + 0, y + 1] = ein"adef, (dc, cefb) -> ab"(projsDL[x - 1, y + 0], C1[x + 0, y + 0], T1[x + 0, y + 0]);
 
-    nT4[x + 0, y + 1] = ein"(afkg, (fejh, egmbi), hlid), jkmcl -> abcd"(projsDL[x + 0, y + 0], T4[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), projsUL[x - 1, y + 0], iPEPS[x + 0, y + 0]);
-    nC4[x + 0, y + 1] = ein"(cd, cafe), defb -> ab"(C4[x + 0, y + 0], T3[x + 0, y + 0], projsUL[x + 0, y + 0]);
+end
+
+function absorption_R!(nC2, nT2, nC3, T1, C2, T2, C3, T3, iPEPS, projsUR, projsDR, (x, y))
+    
+    nC2[x + 0, y - 1] = ein"(adec, cf), fedb -> ab"(T1[x + 0, y + 0], C2[x + 0, y + 0], projsDR[x - 1, y - 1]);
+    nT2[x + 0, y - 1] = ein"(fgjc, (kefh, bgmei), dhil), ajmkl -> abcd"(projsDR[x + 0, y - 1], T2[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), projsUR[x - 1, y - 1], iPEPS[x + 0, y + 0]);
+    nC3[x + 0, y - 1] = ein"bdef, (acef, cd) -> ab"(projsUR[x + 0, y - 1], T3[x + 0, y + 0], C3[x + 0, y + 0]);
+
+end
+
+function absorption_U!(nC1, nT1, nC2, T4, C1, T1, C2, T2, iPEPS, projsUL, projsUR, (x, y))
+    
+    nC1[x - 1, y + 0] = ein"(afec, cd), defb -> ab"(T4[x + 0, y + 0], C1[x + 0, y + 0], projsUR[x + 0, y - 1]);
+    nT1[x - 1, y + 0] = ein"(afjg, (fleh, gcmie), hkid), jbmkl -> abcd"(projsUL[x + 0, y - 1], T1[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), projsUR[x + 0, y + 0], iPEPS[x + 0, y + 0]);
+    nC2[x - 1, y + 0] = ein"adef, (dc, efbc) -> ab"(projsUL[x + 0, y + 0], C2[x + 0, y + 0], T2[x + 0, y + 0]);
+
+end
+
+function absorption_D!(nC3, nT3, nC4, T2, C3, T3, C4, T4, iPEPS, projsDL, projsDR, (x, y))
+    
+    nC3[x - 1, y + 0] = ein"defa, (fecb, dc) -> ab"(projsDL[x - 1, y + 0], T2[x + 0, y + 0], C3[x + 0, y + 0]);
+    nT3[x - 1, y + 0] = ein"(fgja, (fhek, gemic), bhil), jkmld -> abcd"(projsDL[x - 1, y - 1], T3[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), projsDR[x - 1, y + 0], iPEPS[x + 0, y + 0]);
+    nC4[x - 1, y + 0] = ein"(dc, cefb), adef -> ab"(C4[x + 0, y + 0], T4[x + 0, y + 0], projsDR[x - 1, y - 1]);
 
 end
 
 function horizontalCut(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, (x, y))
 
+    # contract the four corners of the TN
     rhoUL = ein"(((ahkg, gi), hcmfj), iljd), kbmel -> abcdef"(T4[x + 0, y + 0], C1[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), T1[x + 0, y + 0], iPEPS[x + 0, y + 0]);
     rhoUR = ein"(((alji, ig), cemhj), khdg), bfmkl -> abcdef"(T1[x + 0, y + 1], C2[x + 0, y + 1], conj(iPEPS[x + 0, y + 1]), T2[x + 0, y + 1], iPEPS[x + 0, y + 1]);
-    @ein rhoU[-1, -2, -3, -4, -5, -6] := rhoUL[-1, -2, -3, 1, 2, 3] * rhoUR[1, 2, 3, -4, -5, -6];
-
     rhoDL = ein"(((ghkd, ig), hjmbf), iajl), klmce -> abcdef"(T4[x + 1, y + 0], C4[x + 1, y + 0], conj(iPEPS[x + 1, y + 0]), T3[x + 1, y + 0], iPEPS[x + 1, y + 0]);
     rhoDR = ein"(((dijl, ig), ejmhb), khga), flmkc -> abcdef"(T3[x + 1, y + 1], C3[x + 1, y + 1], conj(iPEPS[x + 1, y + 1]), T2[x + 1, y + 1], iPEPS[x + 1, y + 1]);
+    
+    # contract upper and lower half
+    @ein rhoU[-1, -2, -3, -4, -5, -6] := rhoUL[-1, -2, -3, 1, 2, 3] * rhoUR[1, 2, 3, -4, -5, -6];
     @ein rhoD[-1, -2, -3, -4, -5, -6] := rhoDR[-1, -2, -3, 1, 2, 3] * rhoDL[1, 2, 3, -4, -5, -6];
 
     return rhoU, rhoD
+
+end
+
+function verticalCut(C1, T1, C2, T2, C3, T3, C4, T4, iPEPS, (x, y))
+
+    # contract the four corners of the TN
+    rhoUL = ein"(((ahkg, gi), hcmfj), iljd), kbmel -> abcdef"(T4[x + 0, y + 0], C1[x + 0, y + 0], conj(iPEPS[x + 0, y + 0]), T1[x + 0, y + 0], iPEPS[x + 0, y + 0]);
+    rhoUR = ein"(((alji, ig), cemhj), khdg), bfmkl -> abcdef"(T1[x + 0, y + 1], C2[x + 0, y + 1], conj(iPEPS[x + 0, y + 1]), T2[x + 0, y + 1], iPEPS[x + 0, y + 1]);
+    rhoDL = ein"(((ghkd, ig), hjmbf), iajl), klmce -> abcdef"(T4[x + 1, y + 0], C4[x + 1, y + 0], conj(iPEPS[x + 1, y + 0]), T3[x + 1, y + 0], iPEPS[x + 1, y + 0]);
+    rhoDR = ein"(((dijl, ig), ejmhb), khga), flmkc -> abcdef"(T3[x + 1, y + 1], C3[x + 1, y + 1], conj(iPEPS[x + 1, y + 1]), T2[x + 1, y + 1], iPEPS[x + 1, y + 1]);
+
+    # contract left and right half
+    @ein rhoL[-1, -2, -3, -4, -5, -6] := rhoDL[-1, -2, -3, 1, 2, 3] * rhoUL[1, 2, 3, -4, -5, -6];
+    @ein rhoR[-1, -2, -3, -4, -5, -6] := rhoUR[-1, -2, -3, 1, 2, 3] * rhoDR[1, 2, 3, -4, -5, -6];
+
+    return rhoL, rhoR
 
 end
 
@@ -332,26 +498,51 @@ d = 2;
 
 latticeTens = Dict{Tuple{Int, Int}, Array{Float64, 5}}();
 for idx = 1 : Lx, idy = 1 : Ly
-    push!(latticeTens, (idx, idy) => randn(chiB, chiB+1, d, chiB, chiB+1));
+    push!(latticeTens, (idx, idy) => randn(chiB, chiB, d, chiB, chiB));
 end
+# push!(latticeTens, (1, 1) => randn(3, 5, d, 4, 6));
+# push!(latticeTens, (2, 1) => randn(4, 6, d, 3, 5));
 iPEPS = pepsUnitCell(Lx, Ly, latticeTens, unitCellLayout);
 
-# initialize CTMRG tensors
-CTMRGTensors = initializeCTMRGTensors(iPEPS, chiE, initMethod = 0);
-C1, T1, C2, T2, C3, T3, C4, T4 = CTMRGTensors;
+# # initialize CTMRG tensors
+# CTMRGTensors = initializeCTMRGTensors(iPEPS, chiE, initMethod = 0);
+# C1, T1, C2, T2, C3, T3, C4, T4 = CTMRGTensors;
 
-T1 = CTMRGTensors[2];
-T3 = CTMRGTensors[6];
-projsUL, projsDL = similar.((T1, T3));
-projsUR, projsDR = similar.((T3, T1));
-nC4, nT4, nC1 = similar.((C4, T4, C1));
+initMethod = 0;
+CTMRGTensors = runCTMRG(iPEPS, chiE, truncBelowE, initMethod)
 
-foreach(keys(iPEPS.tensorDict)) do tensorKey
-    projsUL[tensorKey...], projsDL[tensorKey...], projsUR[tensorKey...], projsDR[tensorKey...] = computeIsometries_LR(CTMRGTensors..., iPEPS, chiE, truncBelowE, tensorKey);
-end
+# #### LR absorption
+# T1 = CTMRGTensors[2];
+# T3 = CTMRGTensors[6];
+# projsUL, projsDL = similar.((T1, T3));
+# projsUR, projsDR = similar.((T3, T1));
+# nC4, nT4, nC1 = similar.((C4, T4, C1));
 
-(oC1, oT1, oT3, oC4, oT4) = deepcopy.((C1, T1, T3, C4, T4));
-foreach(keys(iPEPS.tensorDict)) do tensorKey
-    @info tensorKey
-    absorption_L!(C1, T1, T3, C4, T4, oC1, oT1, oT3, oC4, oT4, iPEPS, projsUL, projsDL, tensorKey);
-end
+# foreach(keys(iPEPS.tensorDict)) do tensorKey
+#     projsUL[tensorKey...], projsDL[tensorKey...], projsUR[tensorKey...], projsDR[tensorKey...] = computeIsometries_LR(CTMRGTensors..., iPEPS, chiE, truncBelowE, tensorKey);
+# end
+
+# (oC1, oT1, oC2, oT2, oC3, oT3, oC4, oT4) = deepcopy.((C1, T1, C2, T2, C3, T3, C4, T4));
+# foreach(keys(iPEPS.tensorDict)) do tensorKey
+#     @info tensorKey
+#     absorption_L!(C1, C4, T4, oC1, oT1, oT3, oC4, oT4, iPEPS, projsUL, projsDL, tensorKey);
+#     absorption_R!(C2, T2, C3, oT1, oC2, oT2, oC3, oT3, iPEPS, projsUR, projsDR, tensorKey);
+# end
+
+# #### UD absorption
+# T1 = CTMRGTensors[2];
+# T3 = CTMRGTensors[6];
+# projsUL, projsUR = similar.((T3, T1));
+# projsDL, projsDR = similar.((T1, T3));
+# nC2, nT2, nC3 = similar.((C2, T2, C3));
+
+# foreach(keys(iPEPS.tensorDict)) do tensorKey
+#     projsUL[tensorKey...], projsUR[tensorKey...], projsDL[tensorKey...], projsDR[tensorKey...] = computeIsometries_UD(CTMRGTensors..., iPEPS, chiE, truncBelowE, tensorKey);
+# end
+
+# (oC1, oT1, oC2, oT2, oC3, oT3, oC4, oT4) = deepcopy.((C1, T1, C2, T2, C3, T3, C4, T4));
+# foreach(keys(iPEPS.tensorDict)) do tensorKey
+#     @info tensorKey
+#     absorption_U!(C1, T1, C2, oT4, oC1, oT1, oC2, oT2, iPEPS, projsUL, projsUR, tensorKey);
+#     absorption_D!(C3, T3, C4, oT2, oC3, oT3, oC4, oT4, iPEPS, projsDL, projsDR, tensorKey);
+# end
