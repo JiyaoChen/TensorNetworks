91e643f92440f9250ce57f5b9b9de3d519040c24
Andreas Haller <halleritis@gmail.com>
2021-08-11 02:51:18 +0200

    minimal working example

--- autodiff/finiteDifferences_test.jl
+++ autodiff/finiteDifferences_test.jl
@@ -6,25 +6,26 @@ using Optim
 # using ChainRulesCore
 using FiniteDifferences
 
-Lx = Ly = 2
-chiB = 2
+chiB = 6
 d = 2
 
-pepsTensors = rand(Float64, Lx, Ly, chiB, chiB, d, chiB, chiB);
+entries = rand(Float64, chiB, chiB, d, chiB, chiB);
+entries /= norm(entries)
 
-function cnorm(pepsTensors)
-    tensor = TensorMap(pepsTensors[1,1,:,:,:,:,:], ℝ^chiB ⊗ ℝ^chiB ⊗ ℝ^d, ℝ^chiB ⊗ ℝ^chiB)
+function cnorm(entries)
+    
+    tensor = TensorMap(entries, ℝ^chiB ⊗ ℝ^chiB ⊗ ℝ^d, ℝ^chiB ⊗ ℝ^chiB)
     norm = @tensor tensor[1,2,3,4,5]*conj(tensor[1,2,3,4,5])
 
-    # tensor = pepsTensors[1,1,:,:,:,:,:]
-    # tensor = reshape(tensor, prod(size(tensor)))
-    # norm = tensor'*tensor
-    return norm
+    return (norm-3)^2+1  # optimal tensor has norm == 3
 end
 
 optimmethod = LBFGS(m = 20);
-optimargs = (Optim.Options(f_tol = 1e-3, show_trace = true), );
+optimargs = (Optim.Options(f_tol = 1e-16, show_trace = true), );
 res = nothing
 let energy = x -> real(cnorm(x))
-    global res = optimize(energy, Δ -> grad(central_fdm(5, 1), cnorm, Δ)[1], pepsTensors, optimmethod, inplace = false, optimargs...);
+    global res = optimize(energy, Δ -> grad(central_fdm(5, 1), cnorm, Δ)[1], entries, optimmethod, inplace = false, optimargs...);
 end
+minimized_norm = reshape(res.minimizer,chiB^4*d)'*reshape(res.minimizer,chiB^4*d)
+@show minimized_norm
+0;  # suppres standard REPL out
